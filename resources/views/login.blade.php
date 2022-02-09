700e7eead9439a0584c5d678ad6684a99eca6372
IAMGOD <sccccccarlet@gmail.com>
2022-02-09 03:26:01 -0800

    login done

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,15 +3,16 @@
 <div class="container custom-login">
 	<div class="row">
 		<div class="col-sm-4 col-md-4 offset-md-4">
-				<form>
+				<form action="/login" method="POST">
+					@csrf
 				  <div class="form-group">
 				    <label for="exampleInputEmail1">Email address</label>
-				    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+				    <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
 				     
 				  </div>
 				  <div class="form-group">
 				    <label for="exampleInputPassword1">Password</label>
-				    <input type="password" class="form-control" id="exampleInputPassword1">
+				    <input type="password" name="password" class="form-control" id="exampleInputPassword1">
 				  </div>
 				  
 				  <button type="submit" class="btn btn-primary">Submit</button>
